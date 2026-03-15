87ffd25417e8a37ccdb30b80853512db363a14e4
SunnyMenkov <spyssasin@gmail.com>
2026-03-16 01:51:59 +0300

    Initial Commit

---
+++ secondlesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondlesson
+//
+//  Created by меньков александр on 16.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
